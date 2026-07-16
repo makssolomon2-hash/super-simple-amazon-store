cb340dbe1f60066474ae404f13780707af1e1abe
Maks Solomon <maks.solomon2@gmail.com>
2026-07-16 08:12:17 +0200

    js product array

---
+++ scripts/amazon.js
@@ -0,0 +1,51 @@
+console.log('hello!');
+
+const products = [
+  {
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    price:'$10.90',
+    rating: {
+      stars: 4.5,
+      count:87,
+    },
+    pricecents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]    
+  },
+  {
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name:'Intermediate Size Basketball',
+    price:'$20.95',
+    rating: {
+      stars: 4,
+      count:127,
+    },
+    pricecents: 2095,
+    keywords: [
+      "ball",
+      "sports",
+      "apparel"
+    ]
+    },
+  {
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    price:'$7.99',
+    rating: {
+      stars: 4.7,
+      count: 224,
+    },
+    pricecents: 799,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]   
+  },
+];
+
+console.log(products);
