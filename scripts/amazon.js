bf9de5af01d1f264cd2f05132bd07fa60ddad277
Maks Solomon <maks.solomon2@gmail.com>
2026-07-18 22:34:43 +0200

    render html in js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
 console.log('hello!');
 
+/* let productsHTML = ''; */
+
 const products = [
   {
     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
@@ -9,7 +11,7 @@ const products = [
       stars: 4.5,
       count:87,
     },
-    pricecents: 1090,
+    priceCents: 1090,
     keywords: [
       "socks",
       "sports",
@@ -24,7 +26,7 @@ const products = [
       stars: 4,
       count:127,
     },
-    pricecents: 2095,
+    priceCents: 2095 ,
     keywords: [
       "ball",
       "sports",
@@ -34,12 +36,27 @@ const products = [
   {
     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
     name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    price:'$7.99',
+    price: 7.99,
     rating: {
-      stars: 4.7,
+      stars: 3 ,
       count: 224,
     },
-    pricecents: 799,
+    priceCents: 889,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]   
+  },
+  {
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    price: 7.99,
+    rating: {
+      stars: 3 ,
+      count: 224,
+    },
+    priceCents: 889,
     keywords: [
       "socks",
       "sports",
@@ -49,3 +66,64 @@ const products = [
 ];
 
 console.log(products);
+
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML +=  `
+  <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+              
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+
+            <div class="product-price">
+              $${product.priceCents / 100}
+            </div>
+
+            <div class="product-quantity-container">
+              <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary">
+              Add to Cart
+            </button>
+          </div>
+  `
+});
+
+console.log(productsHTML);
+
+document.querySelector('.products-grid').innerHTML = productsHTML;
+
