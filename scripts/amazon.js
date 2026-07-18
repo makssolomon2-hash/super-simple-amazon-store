3a928b8f25be3f4f6cb437cad1cf93842c031f70
Maks Solomon <maks.solomon2@gmail.com>
2026-07-18 22:57:14 +0200

    add product data array

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,68 +2,7 @@ console.log('hello!');
 
 /* let productsHTML = ''; */
 
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    price:'$10.90',
-    rating: {
-      stars: 4.5,
-      count:87,
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]    
-  },
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name:'Intermediate Size Basketball',
-    price:'$20.95',
-    rating: {
-      stars: 4,
-      count:127,
-    },
-    priceCents: 2095 ,
-    keywords: [
-      "ball",
-      "sports",
-      "apparel"
-    ]
-    },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    price: 7.99,
-    rating: {
-      stars: 3 ,
-      count: 224,
-    },
-    priceCents: 889,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]   
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    price: 7.99,
-    rating: {
-      stars: 3 ,
-      count: 224,
-    },
-    priceCents: 889,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]   
-  },
-];
+
 
 console.log(products);
 
